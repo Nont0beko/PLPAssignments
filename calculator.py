070aa83ddfd3f74f2de667e7a09d6aa3f23aa5e6
Nont0beko <79195119+Nont0beko@users.noreply.github.com>
2025-03-11 15:37:29 +0200

    "CalculatorAssignment"

---
+++ calculator.py
@@ -0,0 +1,9 @@
+#Allow the user to input 2 integer numbers
+number1 = int(input("Enter the first integer number:"))
+number2 = int(input("Enter the second integer number:"))
+
+#Add 2 numbers together
+result = number1 + number2
+
+#Display result
+print("Your result is:",result)
